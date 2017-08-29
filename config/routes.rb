51fabb0a900aa7ccd2723ed43719c99924a5d34e
Ilona Demidenko <demiluri@gmail.com>
2017-08-29 11:40:27 +0200

    Add tests for dashboard controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'dashboard#dashboard'
+  root 'dashboard#dashboard', as: 'dashboard'
 
   post '/tasks', to: 'tasks#create'
   get '/status', to: 'status#status'
--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'it responds with success' do
+    get dashboard_url
+
+    assert_response :success
+  end
+
+  test 'it loads all tasks' do
+    get dashboard_url
+    actual_tasks = assigns[:tasks].to_a
+
+    expected_tasks = [tasks(:one), tasks(:two)]
+    assert_equal(expected_tasks, actual_tasks)
+  end
 end
